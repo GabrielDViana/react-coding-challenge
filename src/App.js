b174cb394541a42b00dff61c04524c89dfdabce4
Gabriel Viana <gabrielv14@gmail.com>
2024-03-30 15:06:29 -0300

    feature: adding Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import CustomLayout from './containers/Layouts';
+import CustomLayout from './containers/CustomLayout';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <CustomLayout>
-      </CustomLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CustomLayout />}/>
+          <Route path="search" element={<CustomLayout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
--- src/containers/CustomLayout.js
+++ src/containers/CustomLayout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import GalleryContainer from './GalleryContainer';
+import CustomLink from './CustomLink';
 
 class CustomLayout extends React.Component {
 
     render() {
         return (
-            <div className='container'>
-                <GalleryContainer>
-                </GalleryContainer>
+            <div>
+                <div className='container'>
+                    <GalleryContainer>
+                    </GalleryContainer>
+                </div>
             </div>
         );
     }
